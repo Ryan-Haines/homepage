540172d6b8794adca00aebb85764956b961918ce
Ryan Haines <ryan.a.haines@gmail.com>
2014-04-30 00:53:32 -0700

    SVG graphics yay

--- index.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-	//console.log("hello world!!");
-
-	/*
-	$(".contactWrapper").click(function(){
-		window.open('mailto:ryan@ryanhain.es?subject=ryanhain.es visitor contact form&body=Write your message here');
-	});
-	*/
-});
